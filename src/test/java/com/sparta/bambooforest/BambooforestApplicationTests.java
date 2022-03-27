a69b9cf24cfdb5a76164cf7b8740ae54dc983495
hobit22 <hobit22@naver.com>
2022-03-27 23:31:20 +0900

    Initial commit

---
+++ src/test/java/com/sparta/bambooforest/BambooforestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bambooforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooforestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
